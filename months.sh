bc60a5871f98056472c2e5e4350d3d90378ae3c4
C00247797 <89156318+C00247797@users.noreply.github.com>
2021-11-29 16:29:32 +0000

    Add files via upload

--- months.sh
+++ months.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for i in "September" "October" "November" "December"
+for i in September October November December
+
 do
-  echo "The last four months of the year are $i "
+  echo " $i "
 done
