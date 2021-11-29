fd159c283ab4ddcd6627cd23e7bb9c45f997828b
C00247797 <89156318+C00247797@users.noreply.github.com>
2021-11-29 16:24:06 +0000

    Add files via upload

---
+++ months.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+for i in "September" "October" "November" "December"
+do
+  echo "The last four months of the year are $i "
+done
---
+++ numbers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+i=5
+while [ $i -le 95 ]
+do
+  echo " $i "
+  i=$(( $i + 1))
+done
